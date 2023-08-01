2038695d9075f517fd2673f7bc7f2026aff5c9a9
Mohamed Shihan <137610000+MohamedShihanSWIFT@users.noreply.github.com>
2023-08-01 15:45:05 +0400

    Initial Commit

---
+++ Todos/TodosApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodosApp.swift
+//  Todos
+//
+//  Created by Mohamed Shihan on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
